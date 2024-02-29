a73a4e06127ec59cbbff72544322f422700eca54
Michael-Harber <129912256+Michael-Harber@users.noreply.github.com>
2024-02-29 17:56:34 +0000

    updated

--- forms.py
+++ forms.py
@@ -4,6 +4,10 @@ from wtforms import StringField, TextAreaField, FloatField, SelectField, Decimal
 
 from wtforms.validators import InputRequired
 
+from flask_wtf.file import FileField, FileAllowed
+from wtforms.validators import DataRequired
+
+
   #RECIPE ADD FORM
 class RecipeAdd(FlaskForm):
     name = StringField('Name', validators=[InputRequired()])
@@ -35,4 +39,9 @@ class RegistrationForm(FlaskForm):
   password = PasswordField('Password', validators=[validators.DataRequired()])
   confirm_password = PasswordField('Confirm Password', validators=[validators.DataRequired(), validators.EqualTo('password', message='Passwords must match')])
   submit = SubmitField('Register')
+
+# Form for uploading recipe pictures
+class RecipePicForm(FlaskForm):
+    picture = FileField('Recipe Picture', validators=[DataRequired(), FileAllowed(['jpg'], 'Only JPG files allowed.')])
+
 
